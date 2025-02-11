ed75c73c66f7a034c3ef87daef3e118140a19e96
kaitlynle9018 <kaitlynle9018@berkeley.edu>
2025-02-10 21:22:05 -0800

    Adding homework 2

---
+++ homework2/hw2.py
@@ -0,0 +1,21 @@
+# 1) Suppose your current working directory is ~/Desktop/classes/python_decal/. What command would allow you to navigate directly to ~/Desktop?
+# The command would be cd ../..
+# 2) What does ~/ mean?
+# ~ means home directory.
+# 3) What's the difference between an abolute and a relative path in your terminal?
+# The absolute path refers to the entire path to a file while a relative path depends on the current working directory to determine the file location. 
+# 4) What command would bring you back to your home directory?
+# The command would be cd ~.
+# 5) If you called rm ./ in your current working directory, what would it do?
+# It would remove the files in current working directory.
+# 6) In your current working directory, what does calling "git add" do? What about "git commit"? What about "git push"?
+# "git add" stages a specific file. "git commit" commits the changes in GitHub with a message. "git push" pushes the changes to Github.
+# 7) Let's say you call "git add" in your current working directory and then "git status". What message would appear? What is it telling you?
+# "git status" shows the status of the repository, including which files have been modified, which files have been staged, which branch we're on and if it's up to date, and if there are untracked files.
+# 8) What has been the most frustrating error or bug you've encountered in this class so far? How did you troubleshoot and resolve it?
+# Connecting my GitHub repository to my local one. I had to debug it by making a new SSH key and restarting the entire process over again because I had connected my GitHub repository to the wrong folder.
+# 9) How does cloning a repository differ from pulling from a repository?
+# Cloning a repository creates a local copy on our computers while pulling from the repository gets the latest changes.
+# 10) Tell me a fun fact!
+# I have been doing Vietnamese traditional dance for almost 9 years (and still counting!)
+
---
+++ hw1.py
@@ -0,0 +1,15 @@
+#Section 1: Terminal Commands
+#pwd: (1) print working directory, (2) confirms your location in your file system, (3) pwd -P prints the working directory without any symbolic links 
+#cd: (1) changes directory; (2) when you need to change access different files in different directories, (3) cd ~ moves you to the home directory 
+#ls: (1) list elements of directory, (2) when you need to check what files/folders are in the current directory; (3) ls -a lists all files, including hidden files 
+#mkdir: (1) make a new directory/file, (2) when you need to make a new folder, (3) mkdir -p new_folder creates a parent directory
+#cp: (1) copies the directory, (2) when you need to have a file/folder in multiple directories, (3) cp -r folder copies the entire directory
+#mv: (1) moves or renames a file, (2) move the file to another location or rename it, (3) mv your_mom.txt your_dad.txt renames the file
+
+#Section 2: Follow-up questions
+#1: Git is a control system that tracks changes in code, GitHub is a platform that hosts Git repositories for open collabs, and Git Bash is a terminal that runs Git commands on Windows
+#2: The terminal is the interface that allows access to a command-line interface while the command-line itself is the interface that allows the user to type in commands. 
+#3: Windows Powershell utilizes its own scripting language while Git Bash provides Git-based commands on Windows.
+#4: Anaconda is a distribution system that includes Python, conda, etc. conda is a package manager for installing packages on Anaconda. Python is a programming language. 
+#5: Jupyter Notebook is a web-based interface that runs Python while Jupyter Lab is a more advanced version of Jupyter Notebook. 
+#6: VS Code is a code editor with Git support, debugging, and extensions. 
